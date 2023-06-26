c2ff41ea61aba0916c6ffdd8990141f2741b30b0
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-06-26 15:30:48 +0500

    Update functions.c

--- src/functions.c
+++ src/functions.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+void gitClone(const char* inputString) {
+    // Construction of the command string
+    const char* gitCloneCmd = "git clone https://github.com/B-Consortium/";
+    size_t cmdLength = strlen(gitCloneCmd) + strlen(inputString) + 1;
+    char* command = (char*)malloc(cmdLength);
+    snprintf(command, cmdLength, "%s%s", gitCloneCmd, inputString);
+
+    // Execute the command using system()
+    int status = system(command);
+
+    // Check the return value of system() for any errors
+    if (status == 0) {
+        printf("Git clone successful!\n");
+    } else {
+        printf("Git clone failed with error code %d\n", status);
+    }
+
+    // Free the allocated memory
+    free(command);
+}
