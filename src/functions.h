153e1ba6d80251792d13db075a182402c77bbcab
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-06-26 15:29:33 +0500

    Update functions.h

--- src/functions.h
+++ src/functions.h
@@ -1,1 +1,14 @@
+/*
+ * B-Licensed property held by B-Consortium
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the the phiosophical approach of B-License. See LICENSE for details.
+ */
 
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H 1
+
+// Declaration of functions
+void gitClone(const char* inputString)
+
+#endif
