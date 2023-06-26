87e8982672db892a10fc3610f9251a4829378ed8
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-06-26 16:29:11 +0500

    Update aitshal.c

--- src/aitshal.c
+++ src/aitshal.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+
+#include "functions.h"
+
+int main(int argc, char* argv[]){
+   char* repository = argv[1];
+   executeFiglet("Thanks for using aitshal!");
+   executeFiglet("This command is held by B-Consortium");
+    if (repositoryExists(repository)) {
+       // Perform git clone function
+       executeFiglet("Downloading Output");
+       gitClone(repository);
+       executeFiglet("Installation Output");
+    } else {
+      // Handle the case when the repository does not exist
+       executeFiglet("Whoops! Does not exist");
+    }
    return 0;
 }
